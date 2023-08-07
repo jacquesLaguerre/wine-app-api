647e0911c365c7934d594e7c5a7b5e0ff43c4ec4
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-08-07 16:47:59 -0400

    delete function implemented

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { getAllWines, createNewWines } from "./src/wines.js";
+import { getAllWines, createNewWines, deleteWines } from "./src/wines.js";
 
 const app = express();
 app.use(cors());
@@ -10,6 +10,7 @@ app.use(express.json());
 // app.get('/test', (req, res) => res.send('Hello World!'))
 app.get('/wines', getAllWines)
 app.post('/wines', createNewWines)
+app.delete('/wines', deleteWines)
 
 
 export const api = functions.https.onRequest(app);
--- functions/src/wines.js
+++ functions/src/wines.js
@@ -1,8 +1,15 @@
 import dbConnect from "./dbConnect.js";
+import admin from "firebase-admin";
+import * as secrets from "../secrets.js";
+
+admin.initializeApp({
+credential: admin.credential.cert(secrets)
+})
+
 
 
 export function getAllWines (req,res){
-  const db = dbConnect()
+  const db = dbConnect()``
   db.collection ('wines').get()
   .then(collection => {
       const winesArr = collection.docs.map(doc => {
@@ -20,4 +27,22 @@ export function createNewWines(req, res){
   db.collection('wines').add(req.body)
   .then(doc => res.status(201).send({success: true, message: 'Wines created:'}))
 .catch(err => res.status(500).send({success: false, message: err}))
+}
+
+export async function deleteWines(req, res) {
+  try {
+    const { wineId } = req.params;
+    const db = admin.firestore();
+
+    // Get the wine document reference
+    const wineRef = db.collection("wines").doc(wineId);
+
+    // Delete the document
+    await wineRef.delete();
+
+    res.status(202).json({ message: "Wine deleted" });
+  } catch (error) {
+    console.error("Error deleting wine:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
